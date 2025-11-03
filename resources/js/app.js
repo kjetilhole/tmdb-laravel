87abbd7e5a0ae504b0cab0f05b23f9c0500b3f16
kjetilhole <kjetilhole07@gmail.com>
2025-11-03 20:44:40 +0100

    update appjs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
-import './bootstrap';
+import "./bootstrap";
+
+import { createApp } from "vue";
+import MovieDisplay from "./components/MovieDisplay.vue";
+
+const app = createApp({});
+app.component("movie-display", MovieDisplay);
+app.mount("#app");
